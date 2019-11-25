07ff4333f6dbdb1ad6be4c43c6b14cf5baf4762a
Ronaldo France <ronaldo.silva@catolicasc.org.br>
2019-11-24 22:41:18 -0300

    Retirada autenticação VerifyAuth do cadastro sensores e atuadores para teste

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
-app.use('/admin/actuadors', [verifyAuth], adminActuadorsRouter); //[verifyAuth],s
+app.use('/admin/actuadors', adminActuadorsRouter); //[verifyAuth]
 app.use('/users', usersRouter);
 //app.use('/testimonials', testimonialsRouter);
-app.use('/admin/sensors', [verifyAuth], adminSensorsRouter); // [verifyAuth], verify sensor temperatura
+app.use('/admin/sensors', adminSensorsRouter); // [verifyAuth], verify sensor temperatura
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
